e06b9a7f3b493041feb78cf0e7989b10e8ca9478
Waxer59 <hnboyano@gmail.com>
2023-02-19 17:39:35 +0100

    feat redis db

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,35 @@
-import { Module } from '@nestjs/common';
+import {
+  CacheModule,
+  CacheModuleAsyncOptions,
+  CacheStore,
+  Module,
+} from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { redisStore } from 'cache-manager-redis-store';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    CacheModule.registerAsync<CacheModuleAsyncOptions>({
+      isGlobal: true,
+      inject: [ConfigService],
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const store = await redisStore({
+          socket: {
+            host: configService.get('REDIS_HOST'),
+            port: +configService.get('REDIS_PORT'),
+          },
+          username: configService.get('REDIS_USERNAME'),
+          password: configService.get('REDIS_PASSWORD'),
+          ttl: 600, // <-- 600s = 10 mins ( ttl = Time To Live )
+        });
+        return {
+          store: store as unknown as CacheStore,
+        };
+      },
+    }),
+  ],
   controllers: [],
   providers: [],
 })
