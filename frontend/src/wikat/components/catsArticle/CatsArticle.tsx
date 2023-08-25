66fbe7f544c073bfa457e9f0332c7e65b92ec685
Waxer59 <hnboyano@gmail.com>
2023-08-25 03:05:50 +0200

    fix CatsArticle

--- frontend/src/wikat/components/catsArticle/CatsArticle.tsx
+++ frontend/src/wikat/components/catsArticle/CatsArticle.tsx
@@ -10,57 +10,57 @@ export const CatsArticle: React.FC = () => {
         into your life. Here are just a few reasons why you should consider
         getting a cat:
       </p>
-      <h3 className="font-[600] text-[24px] mt-[20px] text-[18px] max-w-[800px] m-auto">
+      <h3 className="font-[600] mt-[20px] text-[18px] max-w-[800px] m-auto">
         1. They are low maintenance
       </h3>
-      <p className="mt-[20px] text-[18px] text-[18px] max-w-[800px] m-auto">
+      <p className="mt-[20px] text-[18px] max-w-[800px] m-auto">
         Compared to other pets, cats require relatively little maintenance. They
         are independent creatures and are perfectly happy to spend long periods
         of time alone. They dont need to be walked or bathed, and They are
         pretty good at grooming themselves. As long as you provide them with
         food, water, and a clean litter box, they will be perfectly content.
       </p>
-      <h3 className="font-[600] text-[24px] mt-[20px] text-[18px] max-w-[800px] m-auto">
+      <h3 className="font-[600] mt-[20px] text-[18px] max-w-[800px] m-auto">
         2. They are good for your health
       </h3>
-      <p className="mt-[20px] text-[18px] text-[18px] max-w-[800px] m-auto">
+      <p className="mt-[20px] text-[18px] max-w-[800px] m-auto">
         Studies have shown that owning a cat can have a positive impact on your
         health. For example, cats have been shown to reduce stress and anxiety,
         lower blood pressure, and even decrease the risk of heart attack and
         stroke. Additionally, the sound of a cats purr has been shown to have a
         calming effect on the human body.
       </p>
-      <h3 className="font-[600] text-[24px] mt-[20px] text-[18px] max-w-[800px] m-auto">
+      <h3 className="font-[600] mt-[20px] text-[18px] max-w-[800px] m-auto">
         3. They are great companions
       </h3>
-      <p className="mt-[20px] text-[18px] text-[18px] max-w-[800px] m-auto">
+      <p className="mt-[20px] text-[18px] max-w-[800px] m-auto">
         Cats may have a reputation for being aloof, but they can actually be
         very affectionate animals. Many cats love to cuddle and will happily sit
         on your lap for hours on end. They are also great playmates and can
         provide hours of entertainment with their antics.
       </p>
-      <h3 className="font-[600] text-[24px] mt-[20px] text-[18px] max-w-[800px] m-auto">
+      <h3 className="font-[600] mt-[20px] text-[18px] max-w-[800px] m-auto">
         4. They are good for the environment
       </h3>
-      <p className="mt-[20px] text-[18px] text-[18px] max-w-[800px] m-auto">
+      <p className="mt-[20px] text-[18px] max-w-[800px] m-auto">
         Cats are natural hunters and can help to control the rodent population
         in your area. Additionally, because they dont require a lot of resources
         or space, they have a smaller environmental footprint than larger pets
         like dogs.
       </p>
-      <h3 className="font-[600] text-[24px] mt-[20px] text-[18px] max-w-[800px] m-auto">
+      <h3 className="font-[600] mt-[20px] text-[18px] max-w-[800px] m-auto">
         5. They are good for your social life
       </h3>
-      <p className="mt-[20px] text-[18px] text-[18px] max-w-[800px] m-auto">
+      <p className="mt-[20px] text-[18px] max-w-[800px] m-auto">
         Owning a cat can actually improve your social life. Cat owners often
         connect with other cat owners and can bond over their shared love of
         feline friends. Additionally, cats can be a great conversation starter
         and can help to break the ice when meeting new people.
       </p>
-      <h3 className="font-[600] text-[24px] mt-[20px] text-[18px] max-w-[800px] m-auto">
+      <h3 className="font-[600] mt-[20px] text-[18px] max-w-[800px] m-auto">
         Conclusion
       </h3>
-      <p className="mt-[20px] text-[18px] mb-[100px] text-[18px] max-w-[800px] m-auto">
+      <p className="mt-[20px] text-[18px] mb-[100px] max-w-[800px] m-auto">
         In conclusion, owning a cat can bring a lot of joy and benefits into
         your life. They are low maintenance, good for your health, great
         companions, good for the environment, and good for your social life. If
