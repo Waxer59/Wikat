4a60a49aae2ec8da625aefcaf8090236a4a2b0b4
Waxer59 <hnboyano@gmail.com>
2023-08-25 03:02:23 +0200

    update eslint

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -3,7 +3,12 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: ['plugin:react/recommended', 'standard-with-typescript', 'prettier'],
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'plugin:react-hooks/recommended',
+    'prettier'
+  ],
   ignorePatterns: ['vite.config.ts'],
   overrides: [],
   parserOptions: {
@@ -13,7 +18,7 @@ module.exports = {
     project: ['./tsconfig.json'],
     tsconfigRootDir: __dirname
   },
-  plugins: ['react'],
+  plugins: ['react', 'react-refresh'],
   rules: {
     'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,5 +1,3 @@
-/// <reference types="vite/client" />
-
 import { type EnvironmentValues } from './vite-env-mode.enum'
 
 interface ImportMetaEnv {
