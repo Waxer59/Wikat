0a5e221f1c70cd5c28156b52965679b9cde2de2a
waxer59 <hnboyano@gmail.com>
2024-04-18 01:14:28 +0200

    update caching service

--- backend/src/caching/caching.service.ts
+++ backend/src/caching/caching.service.ts
@@ -5,18 +5,31 @@ import { CACHE_MANAGER } from '@nestjs/cache-manager';
 @Injectable()
 export class CachingService {
   constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
-  async getCacheKey(key: string): Promise<string> {
-    const value = await this.cacheManager.get(key);
-    return value as unknown as string;
+
+  async getCacheKey(key: string): Promise<string | null> {
+    try {
+      const value = await this.cacheManager.get(key);
+      return value as unknown as string;
+    } catch (error) {
+      return null;
+    }
   }
-  async setCacheKey(key: string, value: string): Promise<any> {
-    const setedValue = await this.cacheManager.set(key, value);
-    return setedValue;
+
+  async setCacheKey(key: string, value: string): Promise<void> {
+    try {
+      await this.cacheManager.set(key, value);
+    } catch (error) {}
   }
+
   async deleteCacheKey(key: string): Promise<void> {
-    await this.cacheManager.del(key);
+    try {
+      await this.cacheManager.del(key);
+    } catch (error) {}
   }
+
   async resetCache(): Promise<void> {
-    await this.cacheManager.reset();
+    try {
+      await this.cacheManager.reset();
+    } catch (error) {}
   }
 }
